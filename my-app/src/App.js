ed5c7b2e45f9ed19b3772b200c15910eb38260ff
helisaarinen <helisaarinen@hotmail.com>
2023-12-15 14:13:27 +0200

    1413

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,6 @@ function App() {
         <p>
           Edit asödlkfjöalsdk <code>src/App.js</code> and save to reload.
         </p>
-        <h1>Otsikko h1</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
